001e9ff2fbfad53e3f955b5c17cd19a4b479db89
Alyssa harper <alyssaharper@Alyssas-Air.home>
2021-06-20 22:24:44 -0400

    Reduced HTML. Started over on JS. Trying new functions to get code to work

--- script.js
+++ script.js
@@ -1,125 +1,80 @@
-// var startEl = document.getElementById("header");
-// var quizEl = document.getElementById("intro");
-var startButtonEl = document.getElementById("startQuiz");
-var introBlockEl = document.getElementById("introBlock");
-var questionBlockEl = document.getElementById("question_block");
-var optionButtonsEl = document.getElementById("optionButtons");
-var questionEl = document.querySelector("#questions");
-var optAEl = document.getElementById("optionA");
-var optBEl = document.getElementById("optionB");
-var optCEl = document.getElementById("optionC");
-var optDEl = document.getElementById("optionD");
-var finalScore = document.querySelector("#finalScore");
-var allDoneEl = $("#allDone")
-var initialInputEl = $("#initial_input");
-var submitEl = $("#submit");
-
-//immediately hides the "all done" script, the submit button, and form
-allDoneEl.hide();
-initialInputEl.hide();
-submitEl.hide();
 
 var score = 0;
+var timer;
+var timeLeft = 0;
+var currentQuestion = 0;
 
 var questions = [
     {
     question: "Commonly used data types DO NOT Include:",
-    answers: [
-        {text: "strings", correct: false},
-        {text: "booleans", correct: false},
-        {text: "alerts", correct: true},
-        {text: "numbers", correct: false}
-    ]
+    answers: [ "strings", "booleans", "alerts", "numbers" ],
+    correct: "alerts"
     },
     {
     question: "The condition in an if / else statement is enclosed within _____.",
-    answers: [
-        {text: "quotes", correct: false},
-        {text: "curly brackets", correct: false},
-        {text: "parenthesis", correct: true},
-        {text: "square brackets", correct: false}
-    ]
+    answers: [ "quotes", "curly brackets", "parenthesis", "square brackets"],
+    correct: "parenthesis"
     },
     {
     question: "Arrays in JavaScript can be used to store _____.",
-    answers: [
-        {text: "numbers and strings", correct: false},
-        {text: "other arrays", correct: false},
-        {text: "booleans", correct: false},
-        {text: "all of the above", correct: true}
-    ]
+    answers: [ "number and strings", "other arrays", "booleans", "all of the above"],
+    correct: "all of the above"
     },
     {
     question: "String values must be enclosed within _____ when being assigned to variables.",
-    answers: [
-        {text: "commas", correct: false},
-        {text: "curly brackets", correct: false},
-        {text: "quotes", correct: true},
-        {text: "parenthesis", correct: false}
-    ]
+    answers: ["commas", "curly brackets", "quotes", "parenthesis"],
+    correct: "quotes"
     },
     {
     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    answers: [
-        {text: "JavaScript", correct: false},
-        {text: "terminal/bash", correct: false},
-        {text: "for loops", correct: false},
-        {text: "console.log", correct: true}
-    ]
+    answers: ["JavaScripts", "terminal/bash", "for loops", "console.log"],
+    correct: "console.log"
     },
 ];
-//creates undefined variables for future function use
-var shuffleQues
-var currentQuesIndex
+
 // when start button is clicked, startGame function runs
 startButtonEl.addEventListener('click', startGame);
-//hides my intro block, shuffles my questions, sets my current question index to 0, shows my question block, getQuestions function runs
+//hides my intro block, creates a timer of 100 seconds and decreases the time by one second
 function startGame() {
 introBlockEl.classList.add('hide');
-shuffleQues = questions.sort(() => Math.random() - .5);
-currentQuesIndex = 0;
 questionBlockEl.classList.remove('hide');
+timeLeft = 100;
+document.getElementById("timer").innerHTML = timeLeft;
+
+timer = setInterval(function() {
+    timeLeft--;
+    document.getElementById("timer").innerHTML = timeLeft;
+    if (timeLeft <=0) {
+    clearInterval(timer);
+    gameOver();
+    }
+}, 100);
 getQuestions();
 };
+
+function gameOver() {
+    clearInterval(timer);
+    questionBlockEl.classList.add('hide');
+    allDoneEl.show();
+    initialInputEl.show();
+    submitEl.show();
+}
 // runs resetBlock function and runs showQues function with argument shuffle questions at the current index
 function getQuestions() {
-    resetBlock();
-    showQues(shuffleQues[currentQuesIndex]);
-};
-//sets question text to text from object. For each answer creates a button and adds in appropriate text from object. Also saves correct answer to the dataset. Creates eventlistener that when clicked on runs chooseAnswer function
-function showQues(question) {
-questionEl.innerText = question.question
-question.answers.forEach(answer => {
-    var button = document.createElement('button')
-    button.innerText = answer.text
-    if (answer.correct) {
-        button.dataset.correct = answer.correct
+    currentQuestion++;
+    if(currentQuestion >= questions.length) {
+        endGame();
+        return;
     }
-    button.addEventListener('click', chooseAnswer)
-    optionButtonsEl.appendChild(button)
-})
-};
-// removed originally created buttons in HTML
-function resetBlock() {
-    while (optionButtonsEl.firstChild) {
-        optionButtonsEl.removeChild(optionButtonsEl.firstChild)
-    }
-}
-// gets the button we selected and then check if it is correct. Then creates a function to see if it is actually set to correct or wrong. Create an array for the answers and then checks if each button is correct or not.
-function chooseAnswer(e) {
-    var selectedOption = e.target;
-    var correct = selectedOption.dataset.correct;
-    checkIfCorrect(document.body, correct)
-    Array.from(optionButtonsEl.children).forEach(button => {
-        checkIfCorrect(button, button.dataset.correct)
-    })
+    questionEl.innerHTML = questions[currentQuestion];
 };
 
-function checkIfCorrect(correct) {
-    if (correct) {
-        score++;
-        console.log('correct');
-    } else {
-        console.log("wrong");
-    }
+function wrong() {
+    timeLeft -= 10;
+    getQuestions();
 };
+
+function correct() {
+    score +=20;
+    getQuestions();
+}
